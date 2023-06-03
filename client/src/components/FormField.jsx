be55d35211ee12a4cfb99adc806236cd499a872c
A1exsandro <alexsandro.acp@gmail.com>
2023-06-03 10:05:21 -0400

    fix: refactor the code

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -29,7 +29,7 @@ value, handleChange, isSurpriseMe, handleSurpriseMe }) => {
         name={name}
         placeholder={placeholder}
         value={value}
-        onchange={handleChange}
+        onChange={handleChange}
         required
         className="bg-gray-50 border border-gray-300 text-gray-900
         text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff]
